ee7e54961e0b264b00d3282667ead79d50ee08da
KarchevBorislav <bkarchev@web.de>
2024-09-12 13:31:36 +0200

    Initializing

---
+++ src/test/java/spring/framework/spfdi/SpfDiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.framework.spfdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpfDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
