b267bc141e789fb0c0929d960ca0c279534b8d64
KarchevBorislav <bkarchev@web.de>
2024-09-12 13:33:58 +0200

    Initializing

--- src/main/java/spring/framework/spfdi/SpfDiApplication.java
+++ src/main/java/spring/framework/spfdi/SpfDiApplication.java
@@ -2,12 +2,19 @@ package spring.framework.spfdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import spring.framework.spfdi.controllers.PersonalController;
 
 @SpringBootApplication
 public class SpfDiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpfDiApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        ApplicationContext ctx =  SpringApplication.run(SpfDiApplication.class, args);
+
+        PersonalController personalController = (PersonalController) ctx.getBean("personalController");
+        String greeting = personalController.helloWorld();
+        System.out.println(greeting);
+    }
 
 }
